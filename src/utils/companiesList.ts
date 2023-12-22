6b97b9dcbe2226551cd9435047ea71a9c966a570
Detkov Vasiliy <fgh.detkov@gmail.com>
2023-12-22 12:14:12 +0300

    add faker library

--- src/utils/companiesList.ts
+++ src/utils/companiesList.ts
@@ -1,6 +1,6 @@
 import { faker as faker } from '@faker-js/faker/locale/ru';
 
-import { type CompanyType, type EmployeeType } from './types';
+import { type CompanyType } from './types';
 
 const randomCompanyName = () => faker.company.name();
 const randomCompanyAddress = () => faker.location.streetAddress(true);
@@ -8,19 +8,24 @@ const randomEmployeeSurname = () => faker.person.lastName();
 const randomEmployeeName = () => faker.person.firstName();
 const randomEmployeePosition = () => faker.person.jobTitle();
 
-const mockEmployeesData: EmployeeType[] = Array.from({ length: 333 }, () => [
-    {
-        surname: randomEmployeeSurname(),
-        name: randomEmployeeName(),
-        position: randomEmployeePosition(),
-    },
-]).flat();
+function randomInteger(min: number, max: number) {
+    return min + Math.round(Math.random() * (max - min));
+}
 
-const mockCompaniesData: CompanyType[] = Array.from({ length: 20 }, () => [
+const mockEmployeesData = () =>
+    Array.from({ length: randomInteger(3, 1000) }, () => [
+        {
+            surname: randomEmployeeSurname(),
+            name: randomEmployeeName(),
+            position: randomEmployeePosition(),
+        },
+    ]).flat();
+
+const mockCompaniesData: CompanyType[] = Array.from({ length: randomInteger(3, 100) }, () => [
     {
         name: randomCompanyName(),
         address: randomCompanyAddress(),
-        employees: mockEmployeesData,
+        employees: mockEmployeesData(),
     },
 ]).flat();
 
